2f3d19eb906e6d3152297127322e79e0807c6dce
Flurop <flora@ratgers.nl>
2026-07-23 16:38:05 +0200

    v2.1

--- src/main/java/nl/orbinuity/dispenserspear/DispenserSpear.java
+++ src/main/java/nl/orbinuity/dispenserspear/DispenserSpear.java
@@ -4,41 +4,48 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.*;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
+import net.minecraft.core.Direction;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.HoverEvent;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.DispenserBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.DispenserBlockEntity;
 
-import java.util.List;
-import java.util.Optional;
+import java.net.URI;
+import java.util.*;
 
 public class DispenserSpear implements ModInitializer {
+	private static final Map<BlockPos, Boolean> lastPowerStatus = new HashMap<>();
+	private static final Map<BlockPos, Long> actionTicks = new HashMap<>();
 	public static final String MODID = "dispenserspear";
 
 	@Override
 	public void onInitialize() {
-		//ServerPlayConnectionEvents.JOIN.register(this::onPlayerJoin);
-		ServerTickEvents.END_WORLD_TICK.register(this::test);
+		ServerPlayConnectionEvents.JOIN.register(this::onPlayerJoin);
+		ServerTickEvents.END_WORLD_TICK.register(this::tickEvent);
 
-		BuiltInRegistries.ITEM.forEach(item -> {
-			DispenserBlock.registerBehavior(item, new DispenseSpearBehavior());
-		});
+		BuiltInRegistries.ITEM.forEach(item -> DispenserBlock.registerBehavior(item, new DispenseSpearBehavior()));
 	}
 
-	private void test(ServerLevel level) {
+	private void tickEvent(ServerLevel level) {
 		for (Entity entity : level.getAllEntities()) {
 			if (entity instanceof Display.ItemDisplay display) {
-				if (display.getTags().contains(DispenseSpearBehavior.dispensedSpearId)) {
+				if (display.getTags().contains(DispenserSpearHelper.dispensedSpearId)) {
 					Optional<String> origin = display.getTags().stream()
 							.filter(s -> s.startsWith("origin:"))
 							.findFirst();
@@ -60,48 +67,101 @@ public class DispenserSpear implements ModInitializer {
 
 					BlockEntity dispenserBlock = level.getBlockEntity(dispenserPos);
 
-					//boolean isNowPowered = level.hasNeighborSignal(dispenserPos);
-					//boolean wasPoweredLastTick = poweredDispensers.contains(dispenserPos);
-
 					if (dispenserBlock instanceof DispenserBlockEntity dispenser) {
-                        /*if (dispenser.isEmpty() && !isNowPowered && wasPoweredLastTick) {
+						if (display.tickCount > 1) {
+							boolean thisLastPowerStatus = lastPowerStatus.get(dispenserPos) != null && lastPowerStatus.get(dispenserPos);
+							if (dispenser.isEmpty() && !thisLastPowerStatus && level.hasNeighborSignal(dispenserPos)) {
+								actionTicks.put(dispenserPos, level.getDayTime()+4);
+							}
+						}
 
-                            Optional<String> itemId = display.getTags().stream()
-                                    .filter(s -> s.startsWith("item_id:"))
-                                    .findFirst();
+						boolean thisActionTicks = actionTicks.get(dispenserPos) != null && actionTicks.get(dispenserPos) <= level.getDayTime();
+						if (thisActionTicks) {
+							actionTicks.remove(dispenserPos);
 
-                            if (itemId.isPresent()) {
-                                Item storedItem = Items.AIR;
-                                String targetId = itemId.get().substring("item_id:".length());
+							ItemStack storedItem = Objects.requireNonNull(display.getSlot(0)).get();
+							ItemStack stack = dispenser.getItem(0);
+							stack.setCount(1);
 
-                                for (Item candidate : ForgeRegistries.ITEMS.getValues()) {
-                                    if (Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(candidate)).toString().equals(targetId)) {
-                                        storedItem = candidate;
-                                        break;
-                                    }
-                                }
-                                for (int i = 0; i < dispenser.getContainerSize(); i++) {
-                                    if (dispenser.getItem(i).isEmpty()) {
-                                        dispenser.setItem(i, new ItemStack(storedItem));
-                                        break;
-                                    }
-                                }
-                            }
-                            display.discard();
-                        }*/
+							if (storedItem.isEmpty()) {
+								storedItem = stack;
+							}
+
+							display.discard();
+
+							boolean isFull = true;
+
+							for (int i = 0; i < dispenser.getContainerSize(); i++) {
+								ItemStack slotItem = dispenser.getItem(i);
+
+								if (slotItem.isEmpty()) {
+									isFull = false;
+									break;
+								}
+							}
+
+							if (!isFull && DispenserSpearHelper.isSpear(stack.getItem())) {
+								dispenser.insertItem(stack);
+							} else {
+								ItemStack refundStack = storedItem.copy();
+
+								Direction side = Direction.UP;
+
+								if (dispenser.getLevel() != null) {
+									Storage<ItemVariant> handler = ItemStorage.SIDED.find(
+											dispenser.getLevel(),
+											dispenser.getBlockPos(),
+											dispenser.getBlockState(),
+											dispenser,
+											side
+									);
+
+									if (handler != null) {
+										long insertedAmount;
+
+										try (Transaction transaction = Transaction.openOuter()) {
+											insertedAmount = handler.insert(
+													ItemVariant.of(refundStack),
+													refundStack.getCount(),
+													transaction
+											);
+											transaction.commit();
+										}
+
+										ItemStack remainder = refundStack.copy();
+										remainder.shrink((int) insertedAmount);
+
+										if (!remainder.isEmpty()) {
+											ItemEntity drop = new ItemEntity(
+													level,
+													dispenserPos.getX() + 0.5,
+													dispenserPos.getY() + 0.5,
+													dispenserPos.getZ() + 0.5,
+													remainder
+											);
+											drop.setDefaultPickUpDelay();
+
+											level.addFreshEntity(drop);
+										}
+									}
+								}
+							}
+						}
+						lastPowerStatus.put(dispenserPos, level.hasNeighborSignal(dispenserPos));
 					} else {
-						ItemStack storedItem = display.getPickResult();
+						ItemStack storedItem = Objects.requireNonNull(display.getSlot(0)).get();
 
-						assert storedItem != null;
-						ItemEntity item = new ItemEntity(
-								level,
-								dispenserPos.getX() + 0.5,
-								dispenserPos.getY() + 0.5,
-								dispenserPos.getZ() + 0.5,
-								storedItem
-						);
+						if (!storedItem.isEmpty()) {
+							ItemEntity item = new ItemEntity(
+									level,
+									dispenserPos.getX() + 0.5,
+									dispenserPos.getY() + 0.5,
+									dispenserPos.getZ() + 0.5,
+									storedItem
+							);
 
-						level.addFreshEntity(item);
+							level.addFreshEntity(item);
+						}
 
 						display.discard();
 					}
@@ -111,7 +171,18 @@ public class DispenserSpear implements ModInitializer {
 	}
 
 
-	/*private void onPlayerJoin(ServerGamePacketListenerImpl serverGamePacketListener, PacketSender packetSender, MinecraftServer minecraftServer) {
-		serverGamePacketListener.getPlayer().displayClientMessage(net.minecraft.network.chat.Component.literal("§6<DispenserSpear>§r Hi! Please note that this is a beta version and is still a work in progress"), false);
-	}*/
+	private void onPlayerJoin(ServerGamePacketListenerImpl serverGamePacketListener, PacketSender packetSender, MinecraftServer minecraftServer) {
+		String latestVersion = DispenserSpearHelper.getLatestVersion("https://data.orbinuity.nl/DispenserSpear/update.json");
+		if (!DispenserSpearHelper.getCurrentVersion().equals(latestVersion)) {
+			Component link = Component.literal("v"+latestVersion)
+					.withStyle(style -> style
+							.withColor(ChatFormatting.BLUE)
+							.withUnderlined(true)
+							.withClickEvent(new ClickEvent.OpenUrl(URI.create("https://orbinuity.nl/project/DispenserSpear")))
+							.withHoverEvent(new HoverEvent.ShowText(Component.literal("Click to open download page")))
+					);
+
+			serverGamePacketListener.getPlayer().displayClientMessage(Component.literal("§6<DispenserSpear>§r Hey! Theres a new version: ").append(link).append(" (Yours: "+DispenserSpearHelper.getCurrentVersion()+")"), false);
+		}
+	}
 }
